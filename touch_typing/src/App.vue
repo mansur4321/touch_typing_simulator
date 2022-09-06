7249e82f3239134ca21f4a17978325ca15ae49e6
mansur4321 <jet_011@mail.ru>
2022-09-06 17:09:10 +0300

    deplay-and-start-label
    развернул проект и добавил первичную верстку перехожу к основной вёрстке

---
+++ touch_typing/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <main-popup></main-popup>
+  </div>
+</template>
+
+<script>
+import mainPopup from "./components/main-popup.vue";
+
+
+export default {
+  name: 'App',
+  components: { 
+    mainPopup 
+  }
+}
+</script>
+
+<style lang="less">
+</style>
