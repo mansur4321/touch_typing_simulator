2d48a31c8ca5f404cb920be6a5b7f03941ad514b
mansur4321 <jet_011@mail.ru>
2022-09-07 23:04:19 +0300

    change-text
    Сделал изменение текста. Немного изменил верстку.

--- touch_typing/src/components/main-popup.vue
+++ touch_typing/src/components/main-popup.vue
@@ -2,7 +2,16 @@
     <div class="main-popup">
         <div class="main-popup__left-column">
             <div class="main-popup__text-field">
-                <span class="main-popup__completed">p[] ghbdtn rfr ltkf xnj ltkftim hahahahah</span><span class="main-popup__selected">a</span><span class="main-popup__empty"> vexla</span>
+                <span class="main-popup__completed">{{completed}}</span>
+                
+                <span
+                    :class="{
+                        'main-popup__selected-error': errorInput,
+                        '_spaceSymbol': spaceSymbol
+                    }" 
+                class="main-popup__selected">{{selected}}</span>
+                
+                <span class="main-popup__empty">{{originalText}}</span>
             </div>
         </div>
 
@@ -35,12 +44,51 @@ export default {
 
     data() {
         return {
+            errorInput: false,
+            spaceSymbol: false,
 
+            completed: '',
+            selected: '',
+            originalText: 'lorems sssssssss sssss ssssssssssss ssssssssssslkd ,clk slkdcl ksdmlckmslk dcmls mdlckmsl kdmclkmsdlkcml sdkmclksdm',
+        }
+    },
+
+    mounted() {
+        document.addEventListener('keydown', (event) => {
+            let symbol = event.key;
+
+            if (symbol.length > 1) return;
+
+            this.updateText(symbol);
+        });
+
+
+        this.selected = this.originalText[0];
+        this.originalText = this.originalText.slice(1);
+    },
+
+    watch: {
+        selected(value) {
+            if (value === ' ') {
+                this.spaceSymbol = true;
+            } else {
+                this.spaceSymbol = false;
+            }
         }
     },
 
     methods: {
-        
+        updateText(symbol) {
+            if (symbol === this.selected) {
+                this.errorInput = false;
+
+                this.completed += this.selected;
+                this.selected = this.originalText[0];
+                this.originalText = this.originalText.slice(1);
+            } else {
+                this.errorInput = true;
+            }
+        }
     }
 }
 </script>
